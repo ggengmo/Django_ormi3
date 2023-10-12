98c2605b87eb17386b1d1cef004181ad62338dec
ggengmo <rudah365dlf@gmail.com>
2023-10-12 16:10:10 +0900

    231012

---
+++ 231012/accounts/accounts/urls.py
@@ -0,0 +1,12 @@
+# accounts > urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('logincheck/', views.logincheck, name='logincheck'),
+    path('loginfbv/', views.loginfbv, name='loginfbv'),
+]
