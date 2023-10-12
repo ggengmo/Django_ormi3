ad074596b8b40be7ac856c47c9dec2058b73ac3f
ggengmo <rudah365dlf@gmail.com>
2023-10-12 23:01:13 +0900

    assignment

---
+++ 231012/mysite2/notice/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Post
+
+@login_required
+def notice(requset):
+    db = Post.objects.all()
+    context = {
+        'db': db,
+    }
+    return render(requset, 'notice/notice.html', context)
+
+def post(request, pk):
+    db = Post.objects.get(pk=pk)
+    context = {
+        'db': db,
+    }
+    return render(request, 'notice/post.html', context)
