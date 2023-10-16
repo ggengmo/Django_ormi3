3b1df5b27072ab3e76849066e407beed4d671146
ggengmo <rudah365dlf@gmail.com>
2023-10-16 15:23:10 +0900

    231016

---
+++ 231016/mysite/blog/admin.py
@@ -0,0 +1,10 @@
+# blog > admin.py
+
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'created_at', 'updated_at']
+    # fields = ['title', 'content']
+
+admin.site.register(Post, PostAdmin)
