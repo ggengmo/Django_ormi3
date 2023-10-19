4dcee8a7b96a7dae89c338ebe12e1bf603f0c690
ggengmo <rudah365dlf@gmail.com>
2023-10-19 17:56:10 +0900

    231019

---
+++ 231019/mysite2/tube/urls.py
@@ -0,0 +1,15 @@
+# tube > urls.py
+from django.urls import path
+from . import views
+
+app_name = 'tube'
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('new/', views.post_new, name='post_new'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path('<int:pk>/comment/new/', views.comment_new, name='comment_new'),
+    path('tag/<str:tag>/', views.posttag, name='posttag'),
+]
