96655bc7d4978d886f28742e7b81f474acf81a62
ggengmo <rudah365dlf@gmail.com>
2023-10-24 23:05:51 +0900

    miniblog

---
+++ miniblog/mysite/blog/admin.py
@@ -0,0 +1,20 @@
+# blog > admin.py
+
+from django.contrib import admin
+from .models import Post, Category, Tag, Comment
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # 자동으로 채워지게 하는 필드
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class TagAdmin(admin.ModelAdmin):
+    # 자동으로 채워지게 하는 필드
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Post)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment)
--- miniblog/mysite/tutorialblog/settings.py
+++ miniblog/mysite/tutorialblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-r7ok!3=h7__ol2b5%bw=jvch(t&0=86g=h#ll!q&^qne$j@(9r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'main',
+    'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = "tutorialblog.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -115,9 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
