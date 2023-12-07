8b5467d1315c51a8eca2f7bddf424a05a3bab85f
ggengmo <rudah365dlf@gmail.com>
2023-12-07 13:34:35 +0900

    users model 수정

--- 231207/users/models.py
+++ 231207/users/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import AbstractUser
 # abstractbaseuser는 password, last_login, is_superuser, username을 기본으로 가지고 있습니다.
 # 초급자에게 권하는 방법은 abstractuser를 사용하는 것입니다. abstractbaseuser는 너무 많은 것을 구현해야 하기 때문입니다.
 
-class user(AbstractUser):
+class User(AbstractUser):
     intro = models.TextField(blank=True)
     profile_image = models.ImageField(blank=True, upload_to='user/images')
     
